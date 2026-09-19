b001c1e466b22282b5a8f7fd192dcac64056e03f
Yisus <esquivelyisus17@gmail.com>
2026-09-19 13:46:50 -0600

    Cambio de icono de app movil final de icono

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "ploopi-v9";
+const CACHE_NAME = "ploopi-v10";
 const OFFLINE_URL = "/offline.html";
 
 // Páginas clave que se guardan apenas se instala el service worker, para que
